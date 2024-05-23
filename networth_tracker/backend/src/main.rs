ed51a2dd2a0a0bfc80a27a1d318c933badcb0963
Cedric Erdelen <cerdelen@c2r3s6.42wolfsburg.de>
2024-05-23 18:57:17 +0200

    database functional as well as networking

---
+++ networth_tracker/backend/src/main.rs
@@ -0,0 +1,38 @@
+mod error;
+mod person;
+
+use actix_web::{App, HttpServer};
+use once_cell::sync::Lazy;
+use surrealdb::engine::remote::ws::Client;
+use surrealdb::engine::remote::ws::Ws;
+use surrealdb::opt::auth::Root;
+use surrealdb::Surreal;
+
+static DB: Lazy<Surreal<Client>> = Lazy::new(Surreal::init);
+
+#[actix_web::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+	DB.connect::<Ws>("surrealdb:8000").await?;
+
+	DB.signin(Root {
+		username: "user",
+		password: "password123",
+	})
+	.await?;
+
+	DB.use_ns("namespace").use_db("database").await?;
+
+	HttpServer::new(|| {
+		App::new()
+			.service(person::create)
+			.service(person::read)
+			.service(person::update)
+			.service(person::delete)
+			.service(person::list)
+	})
+	.bind(("0.0.0.0", 3000))?
+	.run()
+	.await?;
+
+	Ok(())
+}
